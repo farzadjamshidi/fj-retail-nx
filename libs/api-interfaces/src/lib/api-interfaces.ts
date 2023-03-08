8ca2fa2a2f64fec2bcc27cbff14966128818085f
farzadjamshidi <farzadjamshidicom@yahoo.com>
2023-03-09 00:18:05 +0300

    Add json-server library

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -2,3 +2,14 @@ export interface Message
 {
   message: string;
 }
+
+export interface BaseEntity
+{
+  id: string | number | null;
+}
+
+export interface Item extends BaseEntity
+{
+  title: string;
+  description: string;
+}
